fe2e99b74d750bbdaa6203810c8570b0ce467cf3
Sam Svindland <swsvindland@gmail.com>
2023-06-30 17:10:31 -0400

    setup mobile app sign in page

--- apps/expo/src/components/SignInWithOAuth.tsx
+++ apps/expo/src/components/SignInWithOAuth.tsx
@@ -1,23 +1,41 @@
 import { useOAuth } from "@clerk/clerk-expo";
 import React from "react";
-import { Button, View } from "react-native";
+import { Text, Pressable, View } from "react-native";
 import { useWarmUpBrowser } from "../hooks/useWarmUpBrowser";
 
 const SignInWithOAuth = () => {
   useWarmUpBrowser();
 
-  const { startOAuthFlow } = useOAuth({ strategy: "oauth_discord" });
+  const apple = useOAuth({ strategy: "oauth_apple" });
+  const google = useOAuth({ strategy: "oauth_google" });
 
-  const handleSignInWithDiscordPress = React.useCallback(async () => {
+  const handleSignInWithApplePress = React.useCallback(async () => {
     try {
-      const { createdSessionId, signIn, signUp, setActive } =
-        await startOAuthFlow();
+      const { createdSessionId, setActive } = await apple.startOAuthFlow();
       if (createdSessionId) {
         setActive?.({ session: createdSessionId });
       } else {
         // Modify this code to use signIn or signUp to set this missing requirements you set in your dashboard.
-        throw new Error("There are unmet requirements, modifiy this else to handle them")
+        throw new Error(
+          "There are unmet requirements, modifiy this else to handle them",
+        );
+      }
+    } catch (err) {
+      console.log(JSON.stringify(err, null, 2));
+      console.log("error signing in", err);
+    }
+  }, []);
 
+  const handleSignInWithGooglePress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, setActive } = await google.startOAuthFlow();
+      if (createdSessionId) {
+        setActive?.({ session: createdSessionId });
+      } else {
+        // Modify this code to use signIn or signUp to set this missing requirements you set in your dashboard.
+        throw new Error(
+          "There are unmet requirements, modifiy this else to handle them",
+        );
       }
     } catch (err) {
       console.log(JSON.stringify(err, null, 2));
@@ -26,13 +44,24 @@ const SignInWithOAuth = () => {
   }, []);
 
   return (
-    <View className="rounded-lg border-2 border-gray-500 p-4">
-      <Button
-        title="Sign in with Discord"
-        onPress={handleSignInWithDiscordPress}
-      />
+    <View className="flex h-screen items-center justify-center gap-4">
+      <View>
+        <Text className="text-2xl font-bold">Sign In</Text>
+      </View>
+      <Pressable
+        className="flex w-full items-center justify-center rounded-2xl bg-teal-500 p-4 shadow-2xl"
+        onPress={handleSignInWithApplePress}
+      >
+        <Text className="text-white">Sign In with Apple</Text>
+      </Pressable>
+      <Pressable
+        className="flex w-full items-center justify-center rounded-2xl bg-teal-500 p-4 shadow-2xl"
+        onPress={handleSignInWithGooglePress}
+      >
+        <Text className="text-white">Sign In with Google</Text>
+      </Pressable>
     </View>
   );
-}
+};
 
 export default SignInWithOAuth;
--- apps/expo/src/screens/signin.tsx
+++ apps/expo/src/screens/signin.tsx
@@ -6,7 +6,7 @@ import SignInWithOAuth from "../components/SignInWithOAuth";
 
 export const SignInSignUpScreen = () => {
   return (
-    <SafeAreaView className="bg-[#2e026d] bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+    <SafeAreaView className="">
       <View className="h-full w-full p-4">
         <SignInWithOAuth />
       </View>
