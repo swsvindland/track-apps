e51d71c43959374f12819d230a55fa83c46ba611
Sam Svindland <swsvindland@gmail.com>
2023-07-11 16:59:16 -0400

    add app router to nextjs app

---
+++ apps/nextjs/src/app/page.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <main className="flex h-screen flex-col items-center">
+      <div className="container grid grid-cols-1 gap-4">
+        <h1>Home</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
---
+++ apps/nextjs/src/app/privacy/page.tsx
@@ -0,0 +1,173 @@
+import React from "react";
+
+export default function Privacy() {
+  return (
+    <>
+      <main className="p-4">
+        <h2 className="text-2xl">Privacy Policy</h2>
+        <p>
+          Samuel Svindland built his apps, WaterTrack; BodyTrack; PeriodTrack;
+          etc, as an Ad Supported app. This SERVICE is provided by Samuel
+          Svindland at no cost and is intended for use as is.
+        </p>
+        <p>
+          This page is used to inform visitors regarding my policies with the
+          collection, use, and disclosure of Personal Information if anyone
+          decided to use my Service.
+        </p>
+        <p>
+          If you choose to use my Service, then you agree to the collection and
+          use of information in relation to this policy. The Personal
+          Information that I collect is used for providing and improving the
+          Service. I will not use or share your information with anyone except
+          as described in this Privacy Policy.
+        </p>
+        <p>
+          The terms used in this Privacy Policy have the same meanings as in our
+          Terms and Conditions, which is accessible at WaterTrack unless
+          otherwise defined in this Privacy Policy.
+        </p>
+        <p>
+          <strong>Information Collection and Use</strong>
+        </p>
+        <p>
+          For a better experience, while using our Service, I may require you to
+          provide us with certain personally identifiable information, including
+          but not limited to name, email. Any calculations regarding your period
+          cycle, fertility, and ovulation are performed on your device to
+          maintain privacy and is not collected by Samuel Svindland or by any
+          third party service providers.
+        </p>
+        <p>
+          In order to enable cloud sync, default cycle, time zone, flow rate,
+          intimacy, and notes are uploaded to the cloud. This data is not sold
+          or used to track individual users.
+        </p>
+        <p>
+          No information entered into PeriodTrack is ever sold to third parties
+          or used to track individual users. All information that is stored on
+          the cloud is used to create the best user experience and ensure app
+          efficiency, convenience, and accuracy.
+        </p>
+        <p>
+          Upon using the “Delete Account” feature, all user data is
+          automatically removed from all storage points.
+        </p>
+        <div>
+          <p>
+            The app does use third party services that may collect information
+            used to identify you, but should not.
+          </p>
+          <p>
+            Link to privacy policy of third party service providers used by the
+            app
+          </p>
+          <ul>
+            <li>
+              <a
+                href="https://support.google.com/admob/answer/6128543?hl=en"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Google AdMob
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://firebase.google.com/support/privacy/privacy.jsx"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Google Firebase
+              </a>
+            </li>
+          </ul>
+        </div>
+        <p>
+          I may employ third-party companies and individuals due to the
+          following reasons:
+        </p>
+        <ul>
+          <li>To facilitate our Service;</li>
+          <li>To provide the Service on our behalf;</li>
+          <li>To perform Service-related services; or</li>
+          <li>To assist us in analyzing how our Service is used.</li>
+        </ul>
+        <p>
+          I want to inform users of this Service that these third parties have
+          access to your Personal Information. The reason is to perform the
+          tasks assigned to them on our behalf. However, they are obligated not
+          to disclose or use the information for any other purpose.
+        </p>
+        <p>
+          <strong>Security</strong>
+        </p>
+        <p>
+          I value your trust in providing us your Personal Information, thus we
+          are striving to use commercially acceptable means of protecting it.
+          But remember that no method of transmission over the internet, or
+          method of electronic storage is 100% secure and reliable, and I cannot
+          guarantee its absolute security.
+        </p>
+        <p>
+          <strong>Links to Other Sites</strong>
+        </p>
+        <p>
+          This Service may contain links to other sites. If you click on a
+          third-party link, you will be directed to that site. Note that these
+          external sites are not operated by me. Therefore, I strongly advise
+          you to review the Privacy Policy of these websites. I have no control
+          over and assume no responsibility for the content, privacy policies,
+          or practices of any third-party sites or services.
+        </p>
+        <p>
+          <strong>Children’s Privacy</strong>
+        </p>
+        <p>
+          These Services do not address anyone under the age of 13. I do not
+          knowingly collect personally identifiable information from children
+          under 13. In the case I discover that a child under 13 has provided me
+          with personal information, I immediately delete this from our servers.
+          If you are a parent or guardian and you are aware that your child has
+          provided us with personal information, please contact me so that I
+          will be able to do necessary actions.
+        </p>
+        <p>
+          <strong>Changes to This Privacy Policy</strong>
+        </p>
+        <p>
+          I may update our Privacy Policy from time to time. Thus, you are
+          advised to review this page periodically for any changes. I will
+          notify you of any changes by posting the new Privacy Policy on this
+          page.
+        </p>
+        <p>This policy is effective as of 2022-07-01</p>
+        <p>
+          <strong>Contact Us</strong>
+        </p>
+        <p>
+          If you have any questions or suggestions about my Privacy Policy, do
+          not hesitate to contact me at swsvindland@gmail.com.
+        </p>
+        <p>
+          This privacy policy page was created at
+          <a
+            href="https://privacypolicytemplate.net"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            privacypolicytemplate.net
+          </a>
+          and modified/generated by
+          <a
+            href="https://app-privacy-policy-generator.firebaseapp.com/privacy.jsx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            App Privacy Policy Generator
+          </a>
+        </p>
+      </main>
+    </>
+  );
+}
