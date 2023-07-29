317c8f02da2a524de68d3622712f251f513ebe6a
Sam Svindland <swsvindland@gmail.com>
2023-07-29 14:23:41 -0400

    add macro track app

---
+++ apps/macro/src/_app.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { TRPCProvider } from "@acme/utils";
+
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { tokenCache } from "@acme/utils";
+import Constants from "expo-constants";
+import { NavigationContainer } from "@react-navigation/native";
+import { Layout } from "./components/layout";
+import { SignInSignUpScreen } from "@acme/ui";
+
+export const App = () => {
+  return (
+    <ClerkProvider
+      publishableKey={Constants.expoConfig?.extra?.CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
+    >
+      <SignedIn>
+        <TRPCProvider>
+          <SafeAreaProvider>
+            <NavigationContainer>
+              <Layout />
+            </NavigationContainer>
+          </SafeAreaProvider>
+        </TRPCProvider>
+      </SignedIn>
+      <SignedOut>
+        <SignInSignUpScreen logo="bp" />
+      </SignedOut>
+    </ClerkProvider>
+  );
+};
---
+++ apps/macro/src/components/layout.tsx
@@ -0,0 +1,74 @@
+import { FC } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  HomeIcon,
+  Cog6ToothIcon,
+  ChartPieIcon,
+} from "react-native-heroicons/outline";
+import { HomeScreen } from "../screens/home";
+import { ReportsScreen, SettingsScreen } from "@acme/ui";
+import { useColorScheme } from "react-native";
+
+const Tab = createBottomTabNavigator();
+
+export const Layout: FC = () => {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          switch (route.name) {
+            case "Home":
+              return <HomeIcon color={color} size={size} />;
+            case "Reports":
+              return <ChartPieIcon color={color} size={size} />;
+            case "Settings":
+              return <Cog6ToothIcon color={color} size={size} />;
+            default:
+              return null;
+          }
+        },
+        tabBarStyle: {
+          backgroundColor: colorScheme === "dark" ? "#262626" : "#fff",
+        },
+        tabBarActiveTintColor: "teal",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        navigationKey="home"
+        options={{
+          headerStyle: {
+            backgroundColor: colorScheme === "dark" ? "#262626" : "#fff",
+          },
+          headerTintColor: colorScheme === "dark" ? "#fff" : "#000",
+        }}
+      />
+      <Tab.Screen
+        name="Reports"
+        component={ReportsScreen}
+        navigationKey="reports"
+        options={{
+          headerStyle: {
+            backgroundColor: colorScheme === "dark" ? "#262626" : "#fff",
+          },
+          headerTintColor: colorScheme === "dark" ? "#fff" : "#000",
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        navigationKey="settings"
+        options={{
+          headerStyle: {
+            backgroundColor: colorScheme === "dark" ? "#262626" : "#fff",
+          },
+          headerTintColor: colorScheme === "dark" ? "#fff" : "#000",
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
