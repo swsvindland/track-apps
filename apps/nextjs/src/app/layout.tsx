95f5d38f0f598a719268d0557f281b994f17125f
Sam Svindland <swsvindland@gmail.com>
2023-08-07 18:33:36 -0400

    add vercel analytics

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Track",
@@ -9,6 +10,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
